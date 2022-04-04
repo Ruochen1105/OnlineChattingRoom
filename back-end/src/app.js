b1cb0db1aa75fc6972af6e83de156a8f3e299ff6
Ruochen Miao <rm5327@nyu.edu>
2022-04-04 10:33:56 -0400

    skeleton backend

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,6 @@
-const express = require("express") 
-const app = express() 
-
+const express = require("express");
+const app = express();
+const path = require('path');
 
 const databaseURI = process.env.MONGODB_URI;
 
