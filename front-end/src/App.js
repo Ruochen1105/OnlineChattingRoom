50c2d39eae22ef38cac78a1df032ef027749f820
Ruochen Miao <rm5327@nyu.edu>
2022-04-04 22:44:34 -0400

    deal with empty input

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -36,9 +36,10 @@ function App() {
       <form onSubmit={e => e.preventDefault()}>
         <input type = 'text' value={msg} onChange={e => {setMSG(e.target.value)}}/>
         <input type = 'submit' value = 'Post' onClick={e => {
+          if (msg === '') {alert("You can't send nothing!")} else {
           fetch('http://localhost:8080/', {method: 'post', headers: {'Content-Type': 'application/json',}, body: JSON.stringify({message: msg})})
           .then(res => {setMSG('')})
-          .catch((err)=>{console.log(err)})
+          .catch((err)=>{console.log(err)})}
         }}/>
       </form>
     </>
