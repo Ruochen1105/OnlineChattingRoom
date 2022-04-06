29a89336cb14c06669a733e9a266031082a324df
Ruochen Miao <rm5327@nyu.edu>
2022-04-05 21:06:39 -0400

    debugging

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -18,14 +18,14 @@ const log = mongoose.model('log', new mongoose.Schema({
     time: String
 }));
 
-app.post('/board', (req, res) => {
+app.post('/api', (req, res) => {
     const date = new Date();
     const time = (date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "-" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + date.getMilliseconds());
     log.create({content: req.body.message, poster: crypto.randomUUID(), time: time});
     res.json([{}]);
 })
 
-app.get('/board', (req, res) => {
+app.get('/api', (req, res) => {
     log.find({}, (err, varToStoreResult, count) => {
         res.json(varToStoreResult);
     })
@@ -35,4 +35,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'front-end', 'build', 'index.html'))
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
