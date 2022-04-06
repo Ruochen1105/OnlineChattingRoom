d1295a22504291c840fa00b45e2ee35ddd124009
Ruochen Miao <rm5327@nyu.edu>
2022-04-05 21:58:29 -0400

    debugging

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -31,7 +31,7 @@ app.get('/api', (req, res) => {
     })
 })
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'front-end', 'build', 'index.html'))
 })
 
